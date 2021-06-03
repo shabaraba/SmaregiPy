0d0696c95d3861ee44f21eb08df1e91313a980c7
shabaraba <work.shabaraba@gmail.com>
2021-06-03 20:30:30 +0900

    camel case from snake case

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="smaregi-platform-api",
+    name="SmaregiPlatformApi",
     version="0.1.0",
     author="shabaraba",
     author_email="work.shabaraba@gmail.com",
