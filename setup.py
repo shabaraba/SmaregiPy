5bcb62016349db15ab1c4646aa192a4e7fed0e56
shabaraba <work.shabaraba@gmail.com>
2021-06-03 17:54:09 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smaregi-platform-api",
+    version="0.1.0",
+    author="shabaraba",
+    author_email="work.shabaraba@gmail.com",
+    description="Smaregi platform api library. It is unofficial.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shabaraba/smaregi-platform-api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        # 'console_scripts': ['sample_command = sample_command.sample_command:main']
+    },
+    python_requires='>=3.7',
+)
