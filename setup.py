5447e5da1f21b38c0da1a759ee50b982de1522f7
shabaraba <work.shabaraba@gmail.com>
2021-09-17 22:36:16 +0900

    ver0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="smaregipy",
-    version='0.3.0',
+    version='0.3.1',
     author="shabaraba",
     author_email="work.shabaraba@gmail.com",
     description="Smaregi platform api library. It is unofficial.",
